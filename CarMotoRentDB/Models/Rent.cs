3ea1aeb8a77d90f97654870e37a245dc2c7936b3
Qoutel <nezzy124@outlook.com>
2021-12-28 11:11:38 +0200

    ?

--- CarMotoRentDB/Models/Rent.cs
+++ CarMotoRentDB/Models/Rent.cs
@@ -3,8 +3,8 @@
     public class Rent
     {
         public int Id { get; set; }
-        public User Customer { get; set; }
-        public Vehicle Vehicle { get; set; }
+        public User? Customer { get; set; }
+        public Vehicle? Vehicle { get; set; }
         public DateTime RentStartDay { get; set; }
         public DateTime RentEndDay { get; set; }
         public decimal RentAmount { get; set; }
--- CarMotoRentDB/Models/Vehicle.cs
+++ CarMotoRentDB/Models/Vehicle.cs
@@ -4,7 +4,7 @@ namespace CarMotoRentDB.Models
     public class Vehicle
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int YearOfManufactured { get; set; }
         public int Mileage { get; set; }
         public decimal PricePerDay { get; set; }
